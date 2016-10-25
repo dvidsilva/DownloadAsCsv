560fadfea093f7fe44d081fddb8c8ff52c2c6ffe
David Silva <contacto@dvidsilva.com>
2016-10-25 00:44:27 -0700

    improved code style

--- README.md
+++ README.md
@@ -9,13 +9,12 @@ Usage
 Add the csvUtility module as a dependency on your app.
 
     App.module('myApp', ['csvUtility']);
-    
+
 This will allow you to use the directive as an attribute on any element:
 
     <button download-csv data-rows="ArrayWithInformation" data-excluded="fields,to,exclude" data-headers="none" data-ngproperties="include" data-filename="rows.csv" >Download As .csv</button>
-    
-    
-This will add a click event listener to the element that will trigger the download of information in CSV format. 
+
+This will add a click event listener to the element that will trigger the download of information in CSV format.
 
 By default it creates a CSV file whose first row is the name of the properties of every row and excludes additional properties added by them by Angular.
 
@@ -24,15 +23,15 @@ Parameters
 
 The parameters are passed in the Html element as attributes, the only mandatory one is rows.
 
-- **rows** (data-rows="") [array]: An Array of objects that will be exported. 
+- **rows** (data-rows="") [array]: An Array of objects that will be exported.
 Very likely the same one you use in ng-repeat.
 
 - **excluded** (data-excluded="") [optional, string]: Comma separated list of values that should be excluded. This will exclude properties from the objects from columns.
 
-- **headers** (data-headers="") [optional, string]: A comma separated list that will be used to name the headers/columns of the file. If the value is 'none', the first row having the name of the 
+- **headers** (data-headers="") [optional, string]: A comma separated list that will be used to name the headers/columns of the file. If the value is 'none', the first row having the name of the
 properties to identify the columns won't be included.
 
-- **ngproperties** (data-ngproperties="") [optional, string]: If the value of this is 'include' it will include the angular properties added to the array, like $$hashId or $index. 
+- **ngproperties** (data-ngproperties="") [optional, string]: If the value of this is 'include' it will include the angular properties added to the array, like $$hashId or $index.
 
 - **filename** (data-filename="") [optional, string]: the download will trigger the download of a file called report.csv, unless a name is specified.
 
--- downloadascsv.js
+++ downloadascsv.js
@@ -1,123 +1,123 @@
-var csvUtility = angular.module('csvUtility', []);
-csvUtility.directive('downloadCsv', function () {
-  return {
-    restrict: 'AE',
-    replace: 'false',
-    link: function (scope, elem, attrs) {
-      var _debugging, _log, _file, _content, link, download,
-          _ngProperty, _ngProperties, _excluded, excluded, _row;
+(function () {
+  if (!angular) {
+      return;
+  }
+  angular.module('csvUtility', []).directive('downloadCsv', function () {
+      return {
+          restrict: 'A',
+          link: function (scope, elem, attrs) {
+              var _debugging, _log, _content, link, download, _ngProperty, _ngProperties, _excluded, excluded, _row;
 
+              _debugging = true;
+              _log = function _log (msg) {
+                  if (_debugging === true) {
+                      console.log(msg);
+                  }
+              };
 
-      _debugging = true;
-      _log = function (msg) {
-        if (_debugging === true) {
-          console.log(msg);
-        }
+              _ngProperties = ['$$hashKey'];
+              _ngProperty = function _ngProperty(prop, i) {
+                  if (_ngProperties.indexOf(prop, i) === -1) {
+                      return false;
+                  }
+                  return true;
+              };
+
+              _excluded = function _excluded (prop, i) {
+                  if (excluded.indexOf(prop, i) === -1) {
+                      return false;
+                  }
+                  return true;
+              };
+
+              download = function download(_file, filename) {
+                  var blob = new Blob([_file], {
+                      type: 'text/csv;charset=utf-8;'
+                  });
+
+                  filename = filename || "report.csv";
+                  if (filename.match(/\.csv$/gi) === null) {
+                      filename += ".csv";
+                  }
+                  _log(navigator);
+                  if (window.chrome !== void 0) {
+                      _log("using Chrome ");
+                      // Method to use for Chrome 35 and above
+                      blob = URL.createObjectURL(blob);
+                      link = document.createElement("a");
+                      link.setAttribute("href", blob);
+                      link.setAttribute('download', filename);
+                      link.click();
+                      return true;
+                  } else if (  navigator.userAgent.toLowerCase().match(/msie/gi)  || navigator.appName.match(/Internet/gi) || navigator.msMaxTouchPoints !== void 0 ) {
+                      _log("using IE ");
+                      // msMaxTouchPoints is the best way to check for Internet Explorer 11
+                      navigator.msSaveBlob(blob, filename);
+                      return true;
+                  } else {
+                      // Method for every other browser
+                      _log("using other browser");
+                      _file = "data:text/csv;charset=utf-8;" + _file;
+                      _file = encodeURI(_file);
+                      window.open(_file);
+                  }
+              };
+
+              angular.element(elem).on('click', function onClickListenerDownloadAsCSV() {
+                  var prop;
+                  _content = [];
+                  excluded = [];
+                  _row = [];
+                  if (scope[attrs.rows] === "" || scope[attrs.rows] === void 0) {
+                      _log('No  data source specified ');
+                      return false;
+                  }
+                  if (scope[attrs.rows].length < 1) {
+                      _log('the data source is empty');
+                      return false;
+                  }
+                  if (attrs.excluded !== void 0 && attrs.excluded !== "") {
+                      _log('Some properties will be excluded');
+                      excluded = attrs.excluded.split(",");
+                  }
+                  if (attrs.headers === void 0 || attrs.headers !== "none") {
+                      _log('first row will contain headers for the rows.');
+                      for (prop in scope[attrs.rows][0]) {
+                          // Using the property name to call the columns
+                          // Angular properties won't be added unless specified
+                          prop = prop.replace(/,/g, '');
+                          if ((attrs.ngproperties === void 0 || attrs.ngproperties !== 'include') && (_ngProperty(prop) === false && _excluded(prop) === false)) {
+                              _row.push(prop);
+                          }
+                      }
+                  }
+                  _content.push(_row);
+                  _row = [];
+                  for (var i in scope[attrs.rows]) {
+                      // Iterate over all elements of the array
+                      for (prop in scope[attrs.rows][i]) {
+                          // Iterave over every property in each row
+                          prop = prop.replace(/,/g, '');
+                          if ((attrs.ngproperties === void 0 || attrs.ngproperties !== 'include') && (_ngProperty(prop) === false && _excluded(prop) === false)) {
+                              var val = scope[attrs.rows][i][prop].replace(/,/g, '');
+                              _row.push(val);
+                          }
+                      }
+                      _content.push(_row);
+                      _row = [];
+                  }
+                  for (var c in _content) {
+                      _content[c] = _content[c].join(',');
+                  }
+                  _content = _content.join("\n");
+                  // _file += _content;
+                  // Will move the download function somewhere else, is taking too much space here
+                  download(_content, attrs.filename);
+                  return true;
+              });
+              return true;
+          },
       };
+  });
+})();
 
-
-      _ngProperties = ['$$hashKey'];
-      _ngProperty = function (prop, i) {
-        if (_ngProperties.indexOf(prop, i) === -1) {
-          return false;
-        }
-        return true;
-      };
-
-      _excluded = function (prop, i) {
-        if (excluded.indexOf(prop, i) === -1) {
-          return false;
-        }
-        return true;
-      }
-
-      download = function (_file, filename) {
-        var blob = new Blob([_file], {
-          type: 'text/csv;charset=utf-8;'
-        });
-
-        filename = filename || "report.csv";
-        if (filename.match(/\.csv$/gi) === null) {
-          filename += ".csv";
-        }
-        _log(navigator);
-        if (window.chrome !== void 0) {
-          _log("using Chrome ");
-          // Method to use for Chrome 35 and above
-          blob = URL.createObjectURL(blob);
-          link = document.createElement("a");
-          link.setAttribute("href", blob);
-          link.setAttribute('download', filename);
-          link.click();
-          return true;
-        } else if (  navigator.userAgent.toLowerCase().match(/msie/gi)  || navigator.appName.match(/Internet/gi) || navigator.msMaxTouchPoints !== void 0 ) {
-          _log("using IE ");
-          // Method for Internet Explorer
-          // msMaxTouchPoints is the most best way to check for Internet Explorer 11 
-          navigator.msSaveBlob(blob, filename);
-          return true;
-
-        } else {
-          // Method for every other browser
-          _log("using other browser");
-          _file = "data:text/csv;charset=utf-8;" + _file;
-          _file = encodeURI(_file);
-          window.open(_file);
-        }
-      }
-
-      angular.element(elem).on('click', function () {
-        _content = [];
-        excluded = [];
-        _row = [];
-        if (scope[attrs.rows] === "" || scope[attrs.rows] === void 0) {
-          _log('No  data source specified ');
-          return false;
-        }
-        if (scope[attrs.rows].length < 1) {
-          _log('the data source is empty');
-          return false;
-        }
-        if (attrs.excluded !== void 0 && attrs.excluded !== "") {
-          _log('Some properties will be excluded');
-          excluded = attrs.excluded.split(",");
-        }
-        if (attrs.headers === void 0 || attrs.headers !== "none") {
-          _log('first row will contain headers for the rows.');
-          for (var prop in scope[attrs.rows][0]) {
-            // Using the property name to call the columns
-            // Angular properties won't be added unless specified
-            prop = prop.replace(/,/g, '');
-            if ((attrs.ngproperties === void 0 || attrs.ngproperties !== 'include') && (_ngProperty(prop) === false && _excluded(prop) === false)) {
-              _row.push(prop);
-            }
-          }
-        }
-        _content.push(_row);
-        _row = [];
-        for (var i in scope[attrs.rows]) {
-          // Iterate over all elements of the array
-          for (var prop in scope[attrs.rows][i]) {
-            // Iterave over every property in each row 
-            prop = prop.replace(/,/g, '');
-            if ((attrs.ngproperties === void 0 || attrs.ngproperties !== 'include') && (_ngProperty(prop) === false && _excluded(prop) === false)) {
-              var val = scope[attrs.rows][i][prop].replace(/,/g, '');
-              _row.push(val);
-            }
-          }
-          _content.push(_row);
-          _row = [];
-        }
-        for (var i in _content) {
-          _content[i] = _content[i].join(',');
-        }
-        _content = _content.join("\n");
-        // _file += _content;
-        // Will move the download function somewhere else, is taking too much space here
-        download(_content, attrs.filename);
-        return true;
-      });
-      return true;
-    },
-  };
-})
